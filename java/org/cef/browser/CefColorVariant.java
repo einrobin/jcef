9c475de4d4a515e52be23df2b67674688f76d4de
Robin <robin.michalik@hotmail.com>
2026-02-07 22:22:44 +0100

    Add CefColorVariant file

---
+++ java/org/cef/browser/CefColorVariant.java
@@ -0,0 +1,15 @@
+// Copyright (c) 2026 The Chromium Embedded Framework Authors. All rights
+// reserved. Use of this source code is governed by a BSD-style license that
+// can be found in the LICENSE file.
+
+package org.cef.browser;
+
+public enum CefColorVariant {
+    SYSTEM,
+    LIGHT,
+    DARK,
+    TONAL_SPOT,
+    NEUTRAL,
+    VIBRANT,
+    EXPRESSIVE
+}
